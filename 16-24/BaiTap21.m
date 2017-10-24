4782dc9382ff01ebcaed47b28d3e986b4b73515f
Nguyen Minh Tri <tri@mse.vn>
2017-10-23 20:22:10 -0700

    SURF and Harris extract features

---
+++ 16-24/BaiTap21.m
@@ -0,0 +1,16 @@
+function BaiTap21()
+    imgI = imread('cameraman.jpg');
+    arrPointI = detectSURFFeatures(imgI);
+    imgJ = imread('cameraman30.jpg');
+    arrPointJ = detectSURFFeatures(imgJ);
+    [arrfeatureI, arrValidPointI] = extractFeatures(imgI, arrPointI);    
+    [arrfeatureJ, arrValidPointJ] = extractFeatures(imgJ, arrPointJ);
+    arrIndexPair = matchFeatures(arrfeatureI,arrfeatureJ);
+    arrMatchedPointI = arrValidPointI(arrIndexPair(:,1),:);
+    arrMatchedPointJ = arrValidPointJ(arrIndexPair(:,2),:);
+    figure;
+    ax = axes;
+    showMatchedFeatures(imgI, imgJ, arrMatchedPointI, arrMatchedPointJ, 'montage', 'Parent', ax);
+    title(ax, 'Candidate point matches');
+    legend(ax, 'Match points I','Match points J');
+end
